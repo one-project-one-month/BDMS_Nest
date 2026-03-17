217ca4cddc024b4a48bd41e0289faf286e5aba48
Show Wai Yan <showwaiyan555@gmail.com>
2026-03-17 16:14:35 +0800

    feat(appointments): add unified CreateAppointmentDto with type field

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,47 @@
-export class CreateAppointmentDto {}
+import {
+  IsNotEmpty,
+  IsUUID,
+  IsDateString,
+  IsString,
+  IsOptional,
+  Matches,
+  IsEnum,
+} from 'class-validator';
+
+export enum AppointmentType {
+  DONATION = 'donation',
+  REQUEST = 'request',
+}
+
+export class CreateAppointmentDto {
+  @IsNotEmpty()
+  @IsEnum(AppointmentType)
+  type: AppointmentType;
+
+  @IsOptional()
+  @IsUUID()
+  donation_id?: string;
+
+  @IsOptional()
+  @IsUUID()
+  blood_request_id?: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  hospital_id: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  appointment_date: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^([0-1][0-9]|2[0-3]):([0-5][0-9])$/, {
+    message: 'appointment_time must be in HH:MM format',
+  })
+  appointment_time: string;
+
+  @IsOptional()
+  @IsString()
+  remarks?: string;
+}
