894a0388051b3847ffdd5c5c016c3bb4cbd7ed03
Show Wai Yan <showwaiyan555@gmail.com>
2026-03-17 16:15:22 +0800

    feat(appointments): add UpdateAppointmentDto

--- src/appointments/dto/update-appointment.dto.ts
+++ src/appointments/dto/update-appointment.dto.ts
@@ -1,4 +1,18 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAppointmentDto } from './create-appointment.dto';
+import { IsOptional, IsDateString, IsString, Matches } from 'class-validator';
 
-export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {}
+export class UpdateAppointmentDto {
+  @IsOptional()
+  @IsDateString()
+  appointment_date?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^([0-1][0-9]|2[0-3]):([0-5][0-9])$/, {
+    message: 'appointment_time must be in HH:MM format',
+  })
+  appointment_time?: string;
+
+  @IsOptional()
+  @IsString()
+  remarks?: string;
+}
